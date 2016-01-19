bc866c41ce72814d57c27caa52d36efdb4b239d1
Zander Hill <zander@xargs.io>
2016-01-18 21:20:57 -0800

    Add host setting to fix channels

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :clio, Clio.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("HOST"), port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :clio, Clio.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
