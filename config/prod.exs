4953d08b619aaa11e6ae403f5cf6204508e46312
Zander Hill <zander@xargs.io>
2016-01-18 21:26:58 -0800

    Update SSL

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,8 @@ use Mix.Config
 
 config :clio, Clio.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("HOST"), port: 80],
+  url: [host: System.get_env("HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 config :clio, Clio.Endpoint, secret_key_base: System.get_env("SECRET_KEY_BASE")
